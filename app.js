441ffc9d526b436a036df1acd4abf7f938f95369
Kateryna <koljuchka93@gmail.com>
2021-10-24 02:54:21 +0300

    add js

--- app.js
+++ app.js
@@ -63,3 +63,72 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const refs = {
+    galleryUl: document.querySelector('.js-gallery'),
+    modalLightbox: document.querySelector('.js-lightbox'), //div
+    imageLightbox: document.querySelector('.lightbox__image'), //class
+    closeLightbox: document.querySelector('[data-action="close-lightbox"]'), //image
+    overlayLightbox: document.querySelector('.lightbox__overlay'), //background
+
+}
+
+const cardsMarkup = createCardsMarkup(galleryItems);
+
+refs.galleryUl.insertAdjacentHTML('beforeend', cardsMarkup);
+// make gallery cards
+function createCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return`
+      <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+        }
+        )
+        .join('');
+}
+// click - open image
+refs.galleryUl.addEventListener('click', onGalleryItemClick)
+
+function onGalleryItemClick(evt) {
+  const activeGalleryItem = evt.target.classList.contains('gallery__image')
+  if (!activeGalleryItem) {
+    return
+  }
+  evt.preventDefault();
+
+  refs.modalLightbox.classList.add('is-open');
+  setImageLightbox(evt.target.dataset.source, evt.target.alt )
+}
+
+// Close modal window
+refs.overlayLightbox.addEventListener('click', modalClose);
+refs.closeLightbox.addEventListener('click', modalClose);
+
+function modalClose(evt) {
+    refs.modalLightbox.classList.remove('is-open');
+      setImageLightbox("", "")
+}
+// Escape button - close
+window.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+        modalClose();
+    }
+});
+
+function setImageLightbox(src, alt) {
+  refs.imageLightbox.src = src
+    refs.imageLightbox.alt = alt;
+}
